c6db74cb454aff21837ad9bcd7b250cd75f0bc30
Dean Gardiner <gardiner91@gmail.com>
2014-05-03 20:46:39 +1200

    [streaming_server] request ranges, track events, more reliable buffering

--- examples/streaming_server/server.py
+++ examples/streaming_server/server.py
@@ -1,8 +1,10 @@
 from dispatcher import Dispatcher
-from spotify.client import Spotify
 from track_reference import TrackReference
+from util import log_progress
 
 from jinja2 import Environment, FileSystemLoader
+from spotify.client import Spotify
+from threading import Event
 import cherrypy
 import logging
 import os
@@ -19,12 +21,12 @@ class Server(object):
         self.sp = Spotify()
         self.cache = {}
 
-    def on_login(self):
-        log.debug('on_login')
+        self.current = None
+        self.on_login = Event()
 
     def start(self):
         # Spotify
-        self.sp.login(os.environ['USERNAME'], os.environ['PASSWORD'], self.on_login)
+        self.sp.login(os.environ['USERNAME'], os.environ['PASSWORD'], lambda: self.on_login.set())
 
         # CherryPy
         cherrypy.config.update({
@@ -41,15 +43,14 @@ class Server(object):
         cherrypy.engine.start()
 
     def album(self, uri):
-        print 'uri', uri
+        self.on_login.wait() # Wait for login
 
-        album = self.sp.metadata(uri).wait()
-        print 'album.name: %s' % album.name
+        album = self.sp.metadata(uri, async=False)
 
         track_uris = [str(tr.uri) for tr in album.discs[0].tracks]
 
         # TODO multi-get
-        tracks = [self.sp.metadata(track_uri).wait() for track_uri in track_uris]
+        tracks = [self.sp.metadata(track_uri, async=False) for track_uri in track_uris]
 
         for x, track in enumerate(tracks):
             print 'discs[0].tracks[%s].name: %s' % (x, track.name)
@@ -63,6 +64,15 @@ class Server(object):
         )
 
     def track(self, uri):
+        self.on_login.wait()  # Wait for login
+
+        # Update current
+        if self.current:
+            # Track changed, call finish()
+            if uri != self.current.uri:
+                log.debug('Changing tracks, calling finish() on previous track')
+                self.current.finish()
+
         # Create new TrackReference (if one doesn't exist yet)
         if uri not in self.cache:
             log.debug('[%s] Creating new TrackReference' % uri)
@@ -76,44 +86,95 @@ class Server(object):
         # Start download
         tr.fetch()
 
-        cherrypy.response.headers['Content-Type'] = tr.info.getheader('Content-Type')
-        cherrypy.response.headers['Content-Length'] = tr.info.getheader('Content-Length')
+        # Update current
+        self.current = tr
 
-        # Progressively read the stream
-        def stream():
-            position = 0
+        r_start, r_end = self.handle_range(tr)
 
-            chunk_size_min = 6 * 1024
-            chunk_size_max = 10 * 1024
+        # Update headers
+        cherrypy.response.headers['Accept-Ranges'] = 'bytes'
+        cherrypy.response.headers['Content-Type'] = tr.response_headers.getheader('Content-Type')
+        cherrypy.response.headers['Content-Length'] = r_end - r_start
 
-            chunk_scale = 0
-            chunk_size = chunk_size_min
-
-            while True:
-                # Adjust chunk_size
-                if chunk_scale < 1:
-                    chunk_scale = 2 * (float(position) / tr.length)
-                    chunk_size = int(chunk_size_min + (chunk_size_max * chunk_scale))
-
-                    if chunk_scale > 1:
-                        chunk_scale = 1
-
-                # Read chunk
-                log.debug('[%s] read - position: %s, chunk_size: %s' % (tr.uri, position, chunk_size))
-                chunk = tr.read(position, chunk_size)
-
-                if not chunk:
-                    log.debug('[%s] Finished at %s bytes (content-length: %s)' % (tr.uri, position, tr.length))
-                    break
-
-                position = position + len(chunk)
-
-                # Write chunk
-                yield chunk
-
-        return stream()
+        # Progressively return track from buffer
+        return self.stream(tr, r_start, r_end)
 
     track._cp_config = {'response.stream': True}
+
+    @staticmethod
+    def stream(tr, r_start, r_end):
+        position = r_start
+
+        chunk_size_min = 6 * 1024
+        chunk_size_max = 10 * 1024
+
+        chunk_scale = 0
+        chunk_size = chunk_size_min
+
+        last_progress = None
+
+        while True:
+            # Adjust chunk_size
+            if chunk_scale < 1:
+                chunk_scale = 2 * (float(position) / tr.stream_length)
+                chunk_size = int(chunk_size_min + (chunk_size_max * chunk_scale))
+
+                if chunk_scale > 1:
+                    chunk_scale = 1
+
+            if position + chunk_size > r_end:
+                chunk_size = r_end - position
+
+            # Read chunk
+            chunk = tr.read(position, chunk_size)
+
+            if not chunk:
+                log.debug('[%s] Finished at %s bytes (content-length: %s)' % (tr.uri, position, tr.stream_length))
+                break
+
+            last_progress = log_progress(tr, '  Streaming', position, last_progress)
+
+            position = position + len(chunk)
+
+            # Write chunk
+            yield chunk
+
+        log.debug('[%s] Stream Complete', tr.uri)
+
+    def handle_range(self, tr):
+        r_start, r_end = self.parse_range(cherrypy.request.headers.get('Range'))
+
+        if not r_start and not r_end:
+            return 0, tr.stream_length - 1
+
+        if r_end is None or r_end >= tr.stream_length:
+            r_end = tr.stream_length - 1
+
+        log.debug('[%s] Range: %s - %s', tr.uri, r_start, r_end)
+
+        cherrypy.response.headers['Content-Range'] = 'bytes %s-%s/%s' % (r_start, r_end, tr.stream_length)
+        cherrypy.response.status = 206
+
+        return r_start, r_end
+
+    @staticmethod
+    def parse_range(value):
+        value = value.split('=')
+
+        if len(value) != 2:
+            return 0, None
+
+        range_type, range = value
+
+        if range_type != 'bytes':
+            return 0, None
+
+        range = range.split('-')
+
+        if len(range) != 2:
+            return 0, None
+
+        return int(range[0] or 0), int(range[1]) if range[1] else None
 
     def get_track_url(self, uri):
         return "http://%s:%d/track/%s.mp3" % (
--- examples/streaming_server/track_reference.py
+++ examples/streaming_server/track_reference.py
@@ -1,3 +1,7 @@
+from spotify.core.uri import Uri
+from spotify.objects import Track
+from util import log_progress
+
 from threading import Lock, Thread
 from urllib import urlopen
 import logging
@@ -7,22 +11,28 @@ log = logging.getLogger(__name__)
 
 
 class TrackReference(object):
+    buffer_wait_ms = 100
+    buffer_wait = buffer_wait_ms / 1000.0
+
     def __init__(self, server, uri):
         self.server = server
         self.uri = uri
 
         self.response = None
-        self.info = None
+        self.response_headers = None
 
-        self.length = None
+        self.stream_length = None
+        self.stream_info = None
 
         self.reading = False
+        self.playing = False
+        self.finished = False
+
+        self.start_time = None
 
         self.buffer = bytearray()
-        self.buffer_lock = Lock()
 
-        self.read_lock = Lock()
-
+        self.track = None
         self.fetch_lock = Lock()
         self.fetch_thread = None
 
@@ -33,37 +43,50 @@ class TrackReference(object):
 
         self.fetch_lock.acquire()
 
-        # Request metadata
-        self.server.sp.metadata(self.uri)\
-                      .on('success', self.on_metadata)
+        # Create a "shell" track (to access uri functions)
+        self.track = Track.construct(self.server.sp, uri=Uri.from_uri(self.uri))
+
+        # Request the track_uri
+        self.track.track_uri(self.on_track_uri)
 
         if blocking:
             # Wait until we are ready
             self.fetch_lock.acquire()
 
-        self.fetch_thread = Thread(target=self.run)
-        self.fetch_thread.start()
-
         return self.fetch_lock
 
-    def on_metadata(self, track):
-        track.track_uri()\
-             .on('success', self.on_track_uri)
-
     def on_track_uri(self, response):
-        result = response.get('result')
+        self.stream_info = response.get('result')
 
-        if not result or 'uri' not in result:
+        if not self.stream_info or 'uri' not in self.stream_info:
             log.warn('Invalid track_uri response')
             self.fetch_lock.release()
             return
 
-        self.response = urlopen(result['uri'])
-        self.info = self.response.info()
+        self.response = urlopen(self.stream_info['uri'])
+        self.response_headers = self.response.info()
 
-        self.length = int(self.info.getheader('Content-Length'))
+        log.info('Opened "%s"', self.stream_info['uri'])
+        log.info('Info: %s', self.response_headers)
+
+        self.stream_length = int(self.response_headers.getheader('Content-Length'))
+        log.info('Length: %s', self.stream_length)
+
+        if self.response_headers.getheader('Content-Type') == 'text/xml':
+            # Error, log response
+            log.debug(self.response.read())
+        else:
+            # Download track for streaming
+            self.fetch_thread = Thread(target=self.run)
+            self.fetch_thread.start()
 
         self.fetch_lock.release()
+
+        # Request full metadata for future use
+        @self.server.sp.metadata(self.uri)
+        def on_metadata(track):
+            log.debug('received full track metadata')
+            self.track = track
 
     def run(self):
         chunk_size = 1024
@@ -75,35 +98,38 @@ class TrackReference(object):
             chunk = self.response.read(chunk_size)
             #log.debug('[%s] Received %s bytes', self.uri, len(chunk))
 
-            with self.buffer_lock:
-                self.buffer.extend(chunk)
+            self.buffer.extend(chunk)
 
             if not chunk:
                 break
 
-            last_progress = self.update_progress(len(self.buffer), last_progress)
+            last_progress = log_progress(self, 'Downloading', len(self.buffer), last_progress)
 
         self.reading = False
-        log.debug('[%s] Finished', self.uri)
+        log.debug('[%s] Download Complete', self.uri)
 
     def read(self, start, chunk_size=1024):
+        if not self.playing:
+            self.track.track_event(self.stream_info['lid'], 3, 0)
+            self.start_time = time.time()
+            self.playing = True
+
         while self.reading and len(self.buffer) < start + 1:
-            time.sleep(0.5)
+            time.sleep(self.buffer_wait)
 
         return self.buffer[start:start + chunk_size]
 
-    def update_progress(self, position, last_progress):
-        percent = float(position) / self.length
-        value = int(percent * 20)
+    def finish(self):
+        if self.finished:
+            return
 
-        if value == last_progress:
-            return value
+        self.finished = True
 
-        log.debug('[%s] Downloading [%s|%s] %03d%%' % (
-            self.uri,
-            (' ' * value),
-            (' ' * (20 - value)),
-            int(percent * 100)
-        ))
+        position_ms = int((time.time() - self.start_time) * 1000)
 
-        return value
+        if position_ms > self.track.duration:
+            position_ms = self.track.duration
+
+        log.debug('position_ms: %s, duration: %s', position_ms, self.track.duration)
+
+        self.track.track_end(self.stream_info['lid'], position_ms)
