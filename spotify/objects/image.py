1a5ba18d45311cffb2f2b831fe2293402eca4337
Dean Gardiner <gardiner91@gmail.com>
2014-05-10 23:30:40 +1200

    Added 'file_url' property to images

--- spotify/objects/image.py
+++ spotify/objects/image.py
@@ -1,3 +1,4 @@
+from spotify.core import RESOURCE_HOST
 from spotify.core.helpers import convert
 from spotify.core.uri import Uri
 from spotify.objects.base import Descriptor, PropertyProxy
@@ -7,18 +8,27 @@ from spotify.proto import metadata_pb2
 class Image(Descriptor):
     __protobuf__ = metadata_pb2.Image
 
-    file_id = PropertyProxy
-    size = PropertyProxy
+    file_uri = PropertyProxy('file_id', func=lambda file_id: Uri.from_gid('image', file_id))
+
+    size = PropertyProxy()
 
     width = PropertyProxy
     height = PropertyProxy
+
+    @property
+    def file_url(self):
+        if not self.file_uri or type(self.file_uri) is not Uri:
+            return None
+
+        return 'https://%s/%s/%s' % (RESOURCE_HOST, self.width, self.file_uri.to_id())
 
     @classmethod
     def from_dict(cls, sp, data, types):
         uri = Uri.from_id('image', data.get('file_id'))
 
         return cls(sp, {
-            'file_id': uri.to_gid(),
+            'file_uri': uri,
+
             'size': convert(data.get('size'), long),
 
             'width': convert(data.get('width'), long),
