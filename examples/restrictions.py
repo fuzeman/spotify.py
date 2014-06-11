ddee57710cea2ff7b116b87be05500d730d8bcbd
Dean Gardiner <gardiner91@gmail.com>
2014-06-11 20:13:37 +1200

    Fixed forbidden restriction bug

--- examples/restrictions.py
+++ examples/restrictions.py
@@ -8,13 +8,19 @@ log = logging.getLogger(__name__)
 
 class App(object):
     uris = [
+        # "Holes" - "Passenger"
         'spotify:track:6xYcv63zDYiEaJ1KGgysGN',
         'spotify:track:77w4HJEAGzRwHTapyXjFl1',
         'spotify:track:3sgebd31wwbZY8Uyda3yOC',
         'spotify:track:7q3FEfKhqCF3w6Q8uTfDuH',
         'spotify:track:2n9TFOPyTnN0uYdzscNA4g',
         'spotify:track:08du7WH5gMBaCB9TlIpOiI',
-        'spotify:track:2xuw7EWGdn5nYWezDx41xu'
+        'spotify:track:2xuw7EWGdn5nYWezDx41xu',
+
+        # "Robot Rock / Oh Yeah" - "Daft Punk"
+        'spotify:track:5EaZLVO6pSEWIyUDsHWiSU',
+        'spotify:track:0q9zz6nP5izcUnfYndfVX6',
+        'spotify:track:5eS6pTvDNOvh2kyxeZtK3r'
     ]
 
     def __init__(self):
@@ -31,20 +37,21 @@ class App(object):
         results = []
 
         for track in tracks:
-            original_uri = track.uri
+            uri = track.uri
 
             if not track.is_available():
                 track.find_alternative()
 
-            results.append((str(original_uri), track))
+            results.append((str(uri), track))
 
-        for original, track in results:
-            print original, str(track.uri)
+        for uri, track in results:
+            print '[%s] "%s" - "%s"' % (uri, track.name, track.artists[0].name)
+            print '\turi:', str(track.uri)
             print '\tis_available:', track.is_available()
 
             for restriction in track.restrictions:
-                print '\tallowed:', ', '.join(restriction.countries_allowed)
-                print '\tforbidden:', ', '.join(restriction.countries_forbidden)
+                print '\tallowed:', ', '.join(restriction.countries_allowed or [])
+                print '\tforbidden:', ', '.join(restriction.countries_forbidden or [])
 
             print
 
